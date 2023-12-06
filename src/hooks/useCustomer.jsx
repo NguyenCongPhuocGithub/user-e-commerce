fec5a708dfaee373b3b072fad2eee7e2e9e074b6
Cong Phuoc <nguyencongphuoccv@gmail.com>
2023-12-06 19:14:52 +0700

    Upload code

--- src/hooks/useCustomer.jsx
+++ src/hooks/useCustomer.jsx
@@ -2,7 +2,6 @@ import { create } from 'zustand';
 
 const useCustomer = create((set) => ({
   customer: {},
-  // setCustomer: (customer) => set({ customer }),
   setCustomer: (newCustomer) => set((prevState) => ({ customer: { ...prevState.customer, ...newCustomer } })),
 }));
 
